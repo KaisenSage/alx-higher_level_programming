cc06a7685f9c8405eebdb933f5c68169a330f7d1
KaisenSage <kvngbabstar@gmail.com>
2023-12-19 06:31:59 -0800

    0x06-python-classes

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,51 +1,25 @@
-class Square:
-    """Represent a square."""
+#!/usr/bin/python3
+"""Define a MagicClass"""
 
-    def __init__(self, size=0):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
-        """
-        self.size = size
 
-    @property
-    def size(self):
-        """Get/set the current size of the square."""
-        return self.__size
+import math
 
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+
+class MagicClass:
+    """Represents a MagicClass"""
+
+    def __init__(self, radius):
+        self.__radius = 0
+
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
 
     def area(self):
-        """Return the current area of the square."""
-        return self.__size ** 2
+        """Calculate the area of the MagicClass"""
+        return (self.__radius ** 2) * math.pi
 
-    def __eq__(self, other):
-        """Check if two squares are equal in area."""
-        return self.area() == other.area()
-
-    def __ne__(self, other):
-        """Check if two squares are not equal in area."""
-        return self.area() != other.area()
-
-    def __lt__(self, other):
-        """Check if one square has a smaller area than the other."""
-        return self.area() < other.area()
-
-    def __le__(self, other):
-        """Check if one square has a smaller or equal area than the other."""
-        return self.area() <= other.area()
-
-    def __gt__(self, other):
-        """Check if one square has a greater area than the other."""
-        return self.area() > other.area()
-
-    def __ge__(self, other):
-        """Check if one square has a greater or equal area than the other."""
-        return self.area() >= other.area()
+    def circumference(self):
+        """Calculate the circumference of the MagicClass"""
+        return 2 * math.pi * self.__radius
 
